ca08a347f0e07f7fa955cfd70e460c06153e603e
etxu00 <etxu00@gmail.com>
2024-11-11 14:59:14 -0600

    feat: Actualizar configuración de Workbox para incluir archivos XML en el precache

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{json,Identifier,png,ico,svg,webmanifest,html,md,js,css}'
+		'**/*.{json,Identifier,png,xml,ico,svg,webmanifest,html,md,js,css}'
 	],
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
